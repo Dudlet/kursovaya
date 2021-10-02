685a49ce6134d43d2020ec46aefd164ca2fb2503
Dudlet <awertghfhcd@gmail.com>
2021-10-02 19:17:49 +0300

    dobavil funkciu getTableData

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ function init() {
   createHighcharts(tableData);
   setTableEvents(table);
 }
+
+function getTableData(table) {
+    const dataArray = [],
+      countryArray = [],
+      populationArray = [],
+      densityArray = [],
+      ageArray = [];
+  
+    
+    table.rows({ search: "applied" }).every(function() {
+      const data = this.data();
+      countryArray.push(data[0]);
+      populationArray.push(parseInt(data[1].replace(/\,/g, "")));
+      densityArray.push(parseInt(data[2].replace(/\,/g, "")));
+      ageArray.push(parseInt(data[3].replace(/\,/g, "")));
+    });
+  
+    
+    dataArray.push(countryArray, populationArray, densityArray, ageArray);
+  
+    return dataArray;
+  }
