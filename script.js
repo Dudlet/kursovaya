614910767dac033183014c4609f7982cdf9111ac
Timofeev <chiter555586@gmail.com>
2021-10-02 19:53:38 +0300

    dobavil grafik naseleniya

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let draw = false;
+
 init();
 
 
@@ -9,23 +11,98 @@ function init() {
 }
 
 function getTableData(table) {
-    const dataArray = [],
-      countryArray = [],
-      populationArray = [],
-      densityArray = [],
-      ageArray = [];
-  
-    
-    table.rows({ search: "applied" }).every(function() {
-      const data = this.data();
-      countryArray.push(data[0]);
-      populationArray.push(parseInt(data[1].replace(/\,/g, "")));
-      densityArray.push(parseInt(data[2].replace(/\,/g, "")));
-      ageArray.push(parseInt(data[3].replace(/\,/g, "")));
-    });
-  
-    
-    dataArray.push(countryArray, populationArray, densityArray, ageArray);
-  
-    return dataArray;
-  }
+  const dataArray = [],
+    countryArray = [],
+    populationArray = [],
+    densityArray = [],
+    ageArray = [];
+
+  table.rows({ search: "applied" }).every(function() {
+    const data = this.data();
+    countryArray.push(data[0]);
+    populationArray.push(parseInt(data[1].replace(/\,/g, "")));
+    densityArray.push(parseInt(data[2].replace(/\,/g, "")));
+    ageArray.push(parseInt(data[3].replace(/\,/g, "")));
+  });
+
+  dataArray.push(countryArray, populationArray, densityArray, ageArray);
+
+  return dataArray;
+}
+
+function createHighcharts(data) {
+  Highcharts.setOptions({
+    lang: {
+      thousandsSep: ","
+    }
+  });
+
+  Highcharts.chart("chart", {
+    title: {
+      text: "График"
+    },
+    xAxis: [
+      {
+        categories: data[0],
+        labels: {
+          rotation: -45
+        }
+      }
+    ],
+    yAxis: [
+      {
+        title: {
+          text: "Население"
+        }
+      },
+      {
+        title: {
+          text: "Плотность (Ч/Км²)"
+        },
+        min: 0,
+        opposite: true
+      }
+    ],
+    series: [
+      {
+        name: "Население",
+        color: "#0071A7",
+        type: "column",
+        data: data[1],
+        tooltip: {
+          valueSuffix: " M"
+        }
+      },
+    ],
+    tooltip: {
+      shared: true
+    },
+    legend: {
+      backgroundColor: "#ececec",
+      shadow: true
+    },
+    credits: {
+      enabled: false
+    },
+    noData: {
+      style: {
+        fontSize: "16px"
+      }
+    }
+  });
+}
+
+function setTableEvents(table) {
+  table.on("page", () => {
+    draw = true;
+  });
+
+  table.on("draw", () => {
+    if (draw) {
+      draw = false;
+    } else {
+      const tableData = getTableData(table);
+      createHighcharts(tableData);
+    }
+  });
+}
